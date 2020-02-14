787b348c80a06dfb32bb9267449a45e41dc06d76
stefanonegri <39225773+stefanonegri@users.noreply.github.com>
2020-02-14 11:03:56 +0100

    Add files via upload

---
+++ runDemoEI_Main_transf.sh
@@ -0,0 +1,1 @@
+curl -v -X POST --data @requestTransf.json https://wso2demo4574-sampleservicedemoeimain-1-0-0.wso2apps.com/healthcare/categories/surgery/reserve --header "Content-Type:application/json"
